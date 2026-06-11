f37a6368044059fa4fc428431ab780e637d156bc
omprakashsaggam <omprakash.saggam@gmail.com>
2026-06-11 02:05:33 +0200

    new long 1

---
+++ src/examples/demo.java
@@ -0,0 +1,26 @@
+package examples;
+
+public class demo {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int num =3;
+        System.out.println(num);
+
+
+        int a =1;
+
+        float b = 1.2F;
+
+        double c = 1.2;
+
+        byte d = (byte) 1;
+
+        char f = 'h';
+
+        boolean g = true;
+
+        long h = 123456789L;
+    }
+}
---
+++ src/examples/typecasting.java
@@ -0,0 +1,13 @@
+package examples;
+
+public class typecasting {
+    public static void main(String[] args) {
+        double b1 = 20.0;
+        int c=(int)b1;
+        System.out.println(c);
+
+        int a =100;
+        double d=(int)a;
+        System.out.println(d);
+    }
+}
